213425aab65d96fd3eb55528c1f03f9354935b50
dibborah <dibyajyotiborah469@gmail.com>
2024-03-05 21:14:40 +0530

    mental model Three with Quiz

--- 03.js
+++ 03.js
@@ -14,3 +14,50 @@
 // So this type of simpler expressions(Questions) which literally mean themselves are called literals
 // 2 is a number literal 
 // "I am a hat" is a string literal
+
+// Let's say :
+// we console pet
+
+let pet = 2;
+console.log(pet);
+
+// Did we really pass pet variable to console.log
+// No, we can't really pass varibles to a function
+// When we pass variables we ask Js a Question
+// What is the values of the varible ???? let's say pet???
+// So variables can serve as an expressions too !!!
+
+// The left side must be a wire
+// "Wire" which is named as varible which works as a "wire"
+// And the right side must be an expression which results in a value
+
+
+// # Note: Our UNIVERSE is full or wires
+
+// Primitive data types are immutable: They are permanent part of our Js universe
+// They can't be created, destroy, change
+// For ex: String are primitive so we cannot set(changing) a property to a string value
+// But Arrays are not primitive so we can set properties to a Array values
+
+// Variables are not values 
+// They point to values
+// We can change at what values they are pointing at using the assignment ("=") operator.
+
+// Varibles are like wire: "Wires" are not a Js concepts. But they let us imagine how varibles points to values. We we do assigment there is a wire on the left always a wire on the left side during assignment and an expression(resulting in a value) on the right side.
+
+// If you see contradiction donot get discouraged. When there is one understand that there is a deeper truth hidden underneath. Find it !!!
+
+// We are just building mental modals of Js which the React Community shares
+// This may be slight nitpicking but it important for us to have a precise understanding behind the terms we are using to have an more accurate mental modal about the fundamental of our universe
+
+// QUIZ
+
+// varibles donot have type values do
+// So the type of a varible depends upon which value the varible is currently pointing to!!!
+
+
+
+
+
+
+
