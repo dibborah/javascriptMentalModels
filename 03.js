6a569c7620f363e662d041eecd79b97490e8d92d
dibborah <dibyajyotiborah469@gmail.com>
2024-03-05 20:32:14 +0530

    first commit

---
+++ 03.js
@@ -0,0 +1,16 @@
+// The right side of the assignment must be an expression so that the result is always a value. The result of expressions are always a value.
+
+// Expression: Are values which answers are given by Javascript(Js Compiler)
+
+// Expression: A question to JavaScript
+// As question can be both simple and complex so are the Expressions
+
+// What are literals(number, object or string literals)
+// literals are expressions or questions which are themselves the answer
+// For ex: let pet = 2 or let hat = "I am a hat"
+// Here 2 is a expression which result itself is 2
+// Also the expression "I am a hat" result itself as "I am a hat"
+
+// So this type of simpler expressions(Questions) which literally mean themselves are called literals
+// 2 is a number literal 
+// "I am a hat" is a string literal
